c68066eebaaed47b4168489a683fa8e39a29921c
Nathan <nkmacadam@gmail.com>
2020-02-13 15:41:47 -0500

    Website setup

--- webapp/include/abacus.js
+++ webapp/include/abacus.js
@@ -20,6 +20,31 @@ class abacus
 $(document).ready(function(){
   $("#loadButton").click(function(){
     $('#file-input').trigger('click');
-    $("#splashScreen").fadeOut();
   });
 });
+
+document.getElementById('file-input').addEventListener('change', handleFileSelect, false);
+
+function handleFileSelect(evt) {
+  console.log("handling file");
+
+  var file = evt.target.files[0]; // FileList object
+  if (file) {
+    if (file.type != ".json" || file.type != ".abc")
+    {
+      console.log("Invalid file type");
+    }
+      var reader = new FileReader();
+      reader.readAsText(file, "UTF-8");
+      reader.onload = function (evt) {
+          //document.getElementById("fileContents").innerHTML = evt.target.result;
+          console.log(evt.target.result);
+        }
+      reader.onerror = function (evt) {
+          //document.getElementById("fileContents").innerHTML = "error reading file";
+          console.log("error reading file");
+      }
+
+      $("#splashScreen").fadeOut();
+  }
+}
