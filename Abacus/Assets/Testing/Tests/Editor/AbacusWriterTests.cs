02f806a527ae708d2305adc5a62ee49867cd4eaf
Nathan <nkmacadam@gmail.com>
2020-04-15 18:29:30 -0500

    Modified project directories

---
+++ Abacus/Assets/Testing/Tests/Editor/AbacusWriterTests.cs
@@ -0,0 +1,38 @@
+﻿using Abacus.Internal;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+
+namespace Abacus.Tests
+{
+    public class AbacusWriterTests
+    {
+        [SetUp]
+        public void ResetScene()
+        {
+            EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+        }
+
+        [Test]
+        public void LazyInstantiationIsNotNull()
+        {
+            Assert.IsNotNull(AbacusWriter.Instance);
+        }
+
+        [Test]
+        public void TemporalRecordersAreRegistered()
+        {
+            ITemporal temporalRecorder = Substitute.For<ITemporal>();
+            AbacusWriter.Instance.AddRecord(temporalRecorder);
+            Assert.IsNotEmpty(AbacusWriter.Instance.RegisteredTemporals);
+        }
+
+        [Test]
+        public void RecordableRecordersAreRegistered()
+        {
+            IRecordable recordable = Substitute.For<IRecordable>();
+            AbacusWriter.Instance.AddRecord(recordable);
+            Assert.IsNotEmpty(AbacusWriter.Instance.RegisteredRecordables);
+        }
+    }
+}
---
+++ Abacus/Assets/Testing/Tests/Editor/PropertyMetricTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Abacus.Internal;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Abacus.Tests
+{
+    public abstract class PropertyMetricTests<T> where T : IRecordable
+    {
+        //[SetUp]
+        //public void ResetScene()
+        //{
+        //    EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+        //}
+
+        //[Test]
+        //public void PropertyMetricTestsSimplePasses()
+        //{
+        //    // Use the Assert class to test conditions
+        //}
+    }
+}
